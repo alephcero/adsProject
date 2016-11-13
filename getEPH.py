c83703ff9b00837d9df6c6826a5fda6541213d85
alephcero <4lephcero@gmail.com>
2016-11-13 13:45:02 -0500

    female ahora es 1 para mujer 0 para hombre. Close #4

--- getEPH.py
+++ getEPH.py
@@ -233,7 +233,7 @@ def schoolYears(dataset):
 
 
 def categorize(df):
-    df.female = df.female == 2
+    df.female = (df.female == 2).astype(int)
     df.schoolLevel.replace(to_replace=[99], value=[np.nan] , inplace=True, axis=None) 
     df.lastYear.replace(to_replace=[98,99], value=[np.nan, np.nan] , inplace=True, axis=None)
     df.activity.replace(to_replace=[0], value=[np.nan] , inplace=True, axis=None)
