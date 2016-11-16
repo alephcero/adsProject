0fd832f8ae06f15ec0fc2b91e3e5b9ceacdd79f6
alephcero <4lephcero@gmail.com>
2016-11-16 18:19:21 -0500

    no use la otra funcion para dummyes genericas. Nico despues decinos como funciona. Close #6

--- getEPH.py
+++ getEPH.py
@@ -5,6 +5,8 @@ import sys
 import simpledbf
 
 
+
+
 def getEPHdbf(censusstring):
     print ("Downloading", censusstring)
     ### First I will heck that it is not already there
@@ -241,3 +243,13 @@ def categorize(df):
     df.empCond.replace(to_replace=[0], value=[np.nan] , inplace=True, axis=None)
     df.unempCond.replace(to_replace=[0], value=[np.nan] , inplace=True, axis=None)
     return df
+
+
+
+def make_dummy(data):
+    data['male_14to24'] = ((data.female == 0) & ((data.age >= 14) & (data.age <= 24))).astype(int) 
+    data['male_25to34'] = ((data.female == 0) & ((data.age >= 25 ) & ( data.age <= 34))).astype(int)
+    data['female_14to24'] = ((data.female == 1) & ((data.age >= 14 ) & ( data.age <= 24))).astype(int)
+    data['female_25to34'] = ((data.female == 1) & ((data.age >= 25 ) & ( data.age <= 34))).astype(int)
+    data['female_35more'] = ((data.female == 1) & ((data.age >= 35 ))).astype(int)    
+    return data
